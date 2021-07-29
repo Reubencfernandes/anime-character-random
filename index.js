7308b48972c5c6c6b4f3746fadc45e41b248c763
unknown <18reuchagasfernandes@gmail.com>
2021-07-29 11:12:11 +0530

    stable version

---
+++ index.js
@@ -0,0 +1,535 @@
+
+const data = [{
+  difficulty: 'hard',
+  image: "https://www.generatormix.com/images/anime-character/killua-zaoldyeck.jpg",
+  name: 'Killua Zaoldyeck',
+  tags: ['Killua', 'Zaoldyeck']
+},
+{
+  difficulty: 'Medium',
+  image: "https://www.generatormix.com/images/anime-character/king.jpg",
+  name: 'King',
+  tags: ['King','King']
+}, {
+  difficulty: 'easy',
+  image: "https://www.generatormix.com/images/anime-character/kyo-sohma.jpg",
+  name: 'Kakashi Hatake',
+  tags: ['Kakashi', 'Hatake']
+}, {
+  difficulty: 'easy',
+  image: "https://www.coolgenerator.com/Pic/anime/251339.jpg",
+  name: 'Kaneki Ken',
+  tags: ['Kaneki', 'Ken']
+}, {
+  difficulty: 'easy',
+  image: "https://www.coolgenerator.com/Pic/anime/384253.jpg",
+  name: 'Agatsuma Zenitsu',
+  tags: ['Agatsuma', 'Zenitsu']
+}, {
+  difficulty: 'easy',
+  image: "https://www.coolgenerator.com/Pic/anime/253713.jpg",
+  name: 'Meliodas',
+  tags: ['Meliodas','Meliodas']
+}, {
+  difficulty: 'hard',
+  image: "https://www.coolgenerator.com/Pic/anime/282380.jpg",
+  name: 'Kakashi Hatake',
+  tags: ['Kakashi', 'Hatake']
+}, {
+  difficulty: 'easy',
+  image: "https://www.coolgenerator.com/Pic/anime/275843.jpg",
+  name: 'Suzuya Juuzou',
+  tags: ['Suzuya', 'Juuzou']
+}, {
+  difficulty: 'Medium',
+  image: "https://www.coolgenerator.com/Pic/anime/174563.jpg",
+  name: 'Kurapika',
+  tags: ['Kurapika','Kurapika']
+}, {
+  difficulty: 'hard',
+  image: "https://www.coolgenerator.com/Pic/anime/288020.jpg",
+  name: 'Tomori Nao',
+  tags: ['Tomori', 'Nao']
+}, {
+  difficulty: 'medium',
+  image: "https://www.coolgenerator.com/Pic/anime/306631.jpg",
+  name: 'Miyamizu Mitsuha',
+  tags: ['Miyamizu', 'Mitsuha']
+}, {
+  difficulty: 'hard',
+  image: "https://www.coolgenerator.com/Pic/anime/183449.jpg",
+  name: 'Shiina Mashiro',
+  tags: ['Shiina', 'Mashiro']
+}, {
+  difficulty: 'easy',
+  image: "https://www.coolgenerator.com/Pic/anime/323859.jpg",
+  name: 'Norman',
+  tags: ['Norman','Norman']
+}, {
+  difficulty: 'easy',
+  image: "https://www.coolgenerator.com/Pic/anime/30971.jpg",
+  name: 'Amane Misa',
+  tags: ['Amane', 'Misa']
+}, {
+  difficulty: 'easy',
+  image: "https://www.coolgenerator.com/Pic/anime/365282.jpg",
+  name: 'Dragneel, Natsu',
+  tags: ['Dragneel', 'Natsu']
+}, {
+  difficulty: 'easy',
+  image: "https://www.coolgenerator.com/Pic/anime/262053.jpg",
+  name: 'Yuuki Asuna',
+  tags: ['Asuna', 'Yuuki']
+}, {
+  difficulty: 'easy',
+  image: "https://www.coolgenerator.com/Pic/anime/36579.jpg",
+  name: 'Toshiro Hitsugaya',
+  tags: ['Toshiro', 'Hitsugaya']
+}, {
+  difficulty: 'easy',
+  image: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/HitsugayaKubo.png/230px-HitsugayaKubo.png",
+  name: 'Hitsugaya Toushirou',
+  tags: ['Hitsugaya', 'Toushirou']
+}, {
+  difficulty: 'easy',
+  image: "https://www.coolgenerator.com/Pic/anime/89190.jpg",
+  name: 'Kurosaki Ichigo',
+  tags: ['Ichigo', 'Kurosaki']
+}, {
+  difficulty: 'hard',
+  image: "https://www.coolgenerator.com/Pic/anime/85308.jpg",
+  name: 'Sturluson Celty',
+  tags: ['Sturluson', 'Celty']
+}, {
+  difficulty: 'medium',
+  image: "https://www.coolgenerator.com/Pic/anime/258757.jpg",
+  name: 'Trafalgar Law',
+  tags: ['Trafalgar', 'Law']
+}, {
+  difficulty: 'medium',
+  image: "https://www.coolgenerator.com/Pic/anime/378966.jpg",
+  name: 'Misaka Mikoto',
+  tags: ['Misaka', 'Mikoto']
+}, {
+  difficulty: 'hard',
+  image: "https://www.coolgenerator.com/Pic/anime/273283.jpg",
+  name: 'Decim',
+  tags: ['Decim','Decim']
+}, {
+  difficulty: 'easy',
+  image: "https://www.coolgenerator.com/Pic/anime/267813.jpg",
+  name: 'Ban',
+  tags: ['Ban','Ban']
+}, {
+  difficulty: 'hard',
+  image: "https://www.coolgenerator.com/Pic/anime/120945.jpg",
+  name: 'Kuronuma, Sawako',
+  tags: ['Kuronuma', ' Sawako']
+}, {
+  difficulty: 'medium',
+  image: "https://www.coolgenerator.com/Pic/anime/316521.jpg",
+  name: 'Liebert Johan',
+  tags: ['Liebert', 'Johan']
+}, {
+  difficulty: 'medium',
+  image: "https://www.coolgenerator.com/Pic/anime/243919.jpg",
+  name: 'Hinata Shouyou',
+  tags: ['Shouyou', 'Hinata']
+}, {
+  difficulty: 'medium',
+  image: "https://www.coolgenerator.com/Pic/anime/53130.jpg",
+  name: 'Katsuragi Misato',
+  tags: ['Katsuragi', 'Misato']
+}, {
+  difficulty: 'medium',
+  image: "https://www.coolgenerator.com/Pic/anime/257273.jpg",
+  name: 'Takanashi Rikka',
+  tags: ['Takanashi', 'Rikka']
+}, {
+  difficulty: 'easy',
+  image: "https://www.coolgenerator.com/Pic/anime/398261.jpg",
+  name: 'Azusagawa Sakuta',
+  tags: ['Azusagawa', 'Sakuta']
+}, {
+  difficulty: 'medium',
+  image: "https://www.coolgenerator.com/Pic/anime/161005.jpg",
+  name: 'Brook',
+  tags: ['Brook','Brook']
+}, {
+  difficulty: 'easy',
+  image: "https://www.coolgenerator.com/Pic/anime/100534.jpg",
+  name: 'Roronoa Zoro',
+  tags: ['Roronoa', 'Zoro']
+}, {
+  difficulty: 'easy',
+  image: "https://www.coolgenerator.com/Pic/anime/249647.jpg",
+  name: 'Lawliet L',
+  tags: ['Lawliet', 'L']
+}, {
+  difficulty: 'hard',
+  image: "https://www.coolgenerator.com/Pic/anime/149491.jpg",
+  name: 'Urahara Kisuke',
+  tags: ['Urahara', 'Kisuke']
+}, {
+  difficulty: 'hard',
+  image: "https://www.coolgenerator.com/Pic/anime/150265.jpg",
+  name: 'Kenpachi Zaraki',
+  tags: ['Kenpachi','Zaraki']
+}, {
+  difficulty: 'hard',
+  image: "https://www.coolgenerator.com/Pic/anime/207883.jpg",
+  name: 'Accelerator',
+  tags: ['Accelerator','Accelerator']
+}, {
+  difficulty: 'medium',
+  image: "https://www.coolgenerator.com/Pic/anime/203555.jpg",
+  name: 'Hikigaya Hachiman',
+  tags: ['Hikigaya', 'Hachiman']
+}, {
+  difficulty: 'hard',
+  image: "https://www.coolgenerator.com/Pic/anime/44160.jpg",
+  name: 'Michaelis Sebastian',
+  tags: ['Michaelis', 'Sebastian']
+}, {
+  difficulty: 'hard',
+  image: "https://www.coolgenerator.com/Pic/anime/59912.jpg",
+  name: 'Kiryuu Zero',
+  tags: ['Kiryuu', 'Zero']
+}, {
+  difficulty: 'hard',
+  image: "https://www.coolgenerator.com/Pic/anime/274913.jpg",
+  name: 'Akabane Karma',
+  tags: ['Akabane', 'Karma']
+}, {
+  difficulty: 'hard',
+  image: "https://www.coolgenerator.com/Pic/anime/253095.jpg",
+  name: 'Akame',
+  tags: ['Akame','Akame']
+}, {
+  difficulty: 'easy',
+  image: "https://www.coolgenerator.com/Pic/anime/299404.jpg",
+  name: 'Midoriya Izuku',
+  tags: ['Midoriya','Izuku']
+}, {
+  difficulty: 'hard',
+  image: "https://www.coolgenerator.com/Pic/anime/243861.jpg",
+  name: 'Meruem',
+  tags: ['Meruem','Meruem']
+}, {
+  difficulty: 'medium',
+  image: "https://www.coolgenerator.com/Pic/anime/101412.jpg",
+  name: 'Gokou Ruri',
+  tags: ['Gokou','Ruri']
+},{
+  difficulty: 'medium',
+  image: "https://www.coolgenerator.com/Pic/anime/222699.jpg",
+  name: 'Kuriyama Mirai',
+  tags: ['Kuriyama','Mirai']
+},{
+  difficulty: 'Hard',
+  image: "https://www.coolgenerator.com/Pic/anime/78215.jpg",
+  name: 'Kuchiki Rukia',
+  tags: ['Kuchiki','Rukia']
+},{
+  difficulty: 'Hard',
+  image: "https://www.coolgenerator.com/Pic/anime/100098.jpg",
+  name: 'Kuchiki Byakuya',
+  tags: ['Kuchiki','Byakuya']
+}, {
+  difficulty: 'Medium',
+  image: "https://www.coolgenerator.com/Pic/anime/72039.jpg",
+  name: 'Lavi',
+  tags: ['Lavi','Lavi']
+}, {
+  difficulty: 'medium',
+  image: "https://www.coolgenerator.com/Pic/anime/237389.jpg",
+  name: 'Misaki Mei',
+  tags: ['Misaki','Mei']
+}, {
+  difficulty: 'hard',
+  image: "https://www.coolgenerator.com/Pic/anime/303227.jpg",
+  name: 'Kagura',
+  tags: ['Kagura','Kagura']
+}, {
+  difficulty: 'medium',
+  image: "https://www.coolgenerator.com/Pic/anime/360783.jpg",
+  name: 'Tempest Rimuru',
+  tags: ['Tempest','Rimuru']
+},{
+  difficulty: 'easy',
+  image: "https://www.coolgenerator.com/Pic/anime/299595.jpg",
+  name: 'Todoroki Shouto',
+  tags: ['Todoroki','Shouto']
+},{
+  difficulty: 'Medium',
+  image: "https://www.coolgenerator.com/Pic/anime/258381.jpg",
+  name: 'Kuroko Tetsuya',
+  tags: ['Kuroko','Tetsuya']
+},{
+  difficulty: 'Hard',
+  image: "https://www.coolgenerator.com/Pic/anime/72171.jpg",
+  name: 'Walker Allen',
+  tags: ['Walker','Allen']
+},{
+  difficulty: 'Medium',
+  image: "https://www.coolgenerator.com/Pic/anime/328158.jpg",
+  name: 'Yato',
+  tags: ['Yato','Yato']
+},{
+  difficulty: 'Hard',
+  image: "https://www.coolgenerator.com/Pic/anime/202721.jpg",
+  name: 'Senjougahara Hitagi',
+  tags: ['Hitagi','Senjougahara']
+},{
+  difficulty: 'Easy',
+  image: "https://www.coolgenerator.com/Pic/anime/206431.jpg",
+  name: 'Smith Erwin',
+  tags: ['Smith','Erwin']
+},{
+  difficulty: 'Easy',
+  image: "https://www.coolgenerator.com/Pic/anime/73469.jpg",
+  name: 'Hughes Maes',
+  tags: ['Hughes','Maes']
+},{
+  difficulty: 'Easy',
+  image: "https://www.coolgenerator.com/Pic/anime/216895.jpg",
+  name: 'Yeager Eren',
+  tags: ['Yeager','Eren']
+},{
+  difficulty: 'Medium',
+  image: "https://www.coolgenerator.com/Pic/anime/238027.jpg",
+  name: 'Nishinoya Yuu',
+  tags: ['Nishinoya','Yuu']
+},{
+  difficulty: 'Hard',
+  image: "https://www.coolgenerator.com/Pic/anime/303282.jpg",
+  name: 'Shiota Nagisa',
+  tags: ['Shiota','Nagisa']
+},{
+  difficulty: 'Medium',
+  image: "https://www.coolgenerator.com/Pic/anime/315153.jpg",
+  name: 'Natsuki Subaru',
+  tags: ['Natsuki','Subaru']
+},{
+  difficulty: 'Easy',
+  image: "https://www.coolgenerator.com/Pic/anime/329560.jpg",
+  name: 'Hashibira Inosuke',
+  tags: ['Hashibira','Inosuke']
+},{
+  difficulty: 'Medium',
+  image: "https://www.coolgenerator.com/Pic/anime/72738.jpg",
+  name: 'Son Gohan',
+  tags: ['Son','Gohan']
+},{
+  difficulty: 'Hard',
+  image: "https://www.coolgenerator.com/Pic/anime/85334.jpg",
+  name: 'Guts',
+  tags: ['Guts','Guts']
+},{
+  difficulty: 'Hard',
+  image: "https://www.coolgenerator.com/Pic/anime/73810.jpg",
+  name: 'Himura Kenshin',
+  tags: ['Himura','Kenshin']
+},{
+  difficulty: 'Medium',
+  image: "https://www.coolgenerator.com/Pic/anime/303921.jpg",
+  name: 'Satou Kazuma',
+  tags: ['Satou','Kazuma']
+},{
+  difficulty: 'Easy',
+  image: "https://www.coolgenerator.com/Pic/anime/278736.jpg",
+  name: 'Hyuuga Hinata',
+  tags: ['Hyuuga','Hinata']
+},{
+  difficulty: 'Hard',
+  image: "https://www.coolgenerator.com/Pic/anime/245977.jpg",
+  name: 'Oikawa Tooru',
+  tags: ['Oikawa','Tooru']
+},{
+  difficulty: 'Easy',
+  image: "https://www.coolgenerator.com/Pic/anime/417885.jpg",
+  name: 'Kozume Kenma',
+  tags: ['Kozume','Kenma']
+},{
+  difficulty: 'Easy',
+  image: "https://www.coolgenerator.com/Pic/anime/308347.jpg",
+  name: 'Edogawa Conan',
+  tags: ['Edogawa','Conan']
+},{
+  difficulty: 'Easy',
+  image: "https://cdn-0.generatormix.com/images/anime-character/kirito.jpg",
+  name: 'Kirito',
+  tags: ['Kirito','Kirito']
+},{
+  difficulty: 'Medium',
+  image: "https://cdn-0.generatormix.com/images/anime-character/momo-yaoyorozu.jpg",
+  name: 'Momo Yaoyorozu',
+  tags: ['Momo','Yaoyorozu']
+}
+,{
+  difficulty: 'Medium',
+  image: "https://cdn-0.generatormix.com/images/anime-character/nami.jpg",
+  name: 'Nami',
+  tags: ['Nami','Nami']
+},{
+  difficulty: 'Medium',
+  image: "https://www.generatormix.com/images/anime-character/gajeel-redfox.jpg",
+  name: 'Gajeel Redfox',
+  tags: ['Gajeel','Redfox']
+},{
+  difficulty: 'Medium',
+  image: "https://www.generatormix.com/images/anime-character/sanji.jpg",
+  name: 'Sanji',
+  tags: ['Sanji','Sanji']
+},{
+  difficulty: 'easy',
+  image: "https://www.generatormix.com/images/anime-character/vegeta.jpg",
+  name: 'Vegeta',
+  tags: ['Vegeta','Vegeta']
+},{
+  difficulty: 'Hard',
+  image: "https://www.generatormix.com/images/anime-character/yatogami.jpg",
+  name: 'Yatogami',
+  tags: ['Yatogami','Yatogami']
+},{
+  difficulty: 'Easy',
+  image: "https://www.generatormix.com/images/anime-character/armin-arlelt.jpg",
+  name: 'Armin Arlelt',
+  tags: ['Armin','Arlelt']
+},{
+  difficulty: 'Medium',
+  image: "https://www.generatormix.com/images/anime-character/juvia-lockser.jpg",
+  name: 'Juvia Lockser',
+  tags: ['Juvia','Lockser']
+}
+,{
+  difficulty: 'Medium',
+  image: "https://www.generatormix.com/images/anime-character/roy-mustang.jpg",
+  name: 'Roy Mustang',
+  tags: ['Roy','Mustang']
+},{
+  difficulty: 'Hard',
+  image: "https://www.generatormix.com/images/anime-character/korosensei.jpg",
+  name: 'Korosensei',
+  tags: ['Korosensei','Korosensei']
+},{
+  difficulty: 'Hard',
+  image: "https://www.generatormix.com/images/anime-character/kenpachi-zaraki.jpg",
+  name: 'Kenpachi Zaraki',
+  tags: ['Kenpachi','Zaraki']
+},{
+  difficulty: 'Medium',
+  image: "https://www.generatormix.com/images/anime-character/saber.jpg",
+  name: 'Saber',
+  tags: ['Saber','Saber']
+},{
+  difficulty: 'easy',
+  image: "https://www.generatormix.com/images/anime-character/katuski-bakugo.jpg",
+  name: 'Katuski Bakugo',
+  tags: ['Katuski','Bakugo']
+},{
+  difficulty: 'Hard',
+  image: "https://www.generatormix.com/images/anime-character/killer-bee.jpg",
+  name: 'Killer Bee',
+  tags: ['Killer','Bee']
+},{
+  difficulty: 'easy',
+  image: "https://www.generatormix.com/images/anime-character/lucy-heartfilia.jpg",
+  name: 'Lucy Heartfilia',
+  tags: ['Lucy',' Heartfilia']
+},{
+  difficulty: 'easy',
+  image: "https://www.generatormix.com/images/anime-character/izuki-(deku)-midoriya.jpg",
+  name: 'Izuki Midoriya',
+  tags: ['Izuki','Midoriya']
+},{
+  difficulty: 'easy',
+  image: "https://www.generatormix.com/images/anime-character/gray-fullbuster.jpg",
+  name: 'Gray Fullbuster',
+  tags: ['Gray','Fullbuster']
+},{
+  difficulty: 'Hard',
+  image: "https://www.generatormix.com/images/anime-character/alucard.jpg",
+  name: 'Alucard',
+  tags: ['Alucard','Alucard']
+}
+,{
+  difficulty: 'easy',
+  image: "https://cdn-0.generatormix.com/images/anime-character/charizard.jpg",
+  name: 'Charizard',
+  tags: ['Charizard','Charizard']
+}
+, {
+  difficulty: 'Medium',
+  image: "https://www.coolgenerator.com/Pic/anime/273755.jpg",
+  name: 'Arima, Kousei',
+  tags: ['Arima', 'Kousei']
+}, {
+  difficulty: 'Hard',
+  image: "https://www.coolgenerator.com/Pic/anime/41081.jpg",
+  name: 'Sesshoumaru',
+  tags: ['Sesshoumaru', 'Sesshoumaru']
+}, {
+  difficulty: 'Easy',
+  image: "https://www.coolgenerator.com/Pic/anime/75175.jpg",
+  name: 'Evans Soul Eater',
+  tags: ['Evans', 'Soul Eater']
+}, {
+  difficulty: 'Medium',
+  image: "https://www.coolgenerator.com/Pic/anime/366718.jpg",
+  name: 'Nakano Miku',
+  tags: ['Nakano', 'Miku']
+}, {
+  difficulty: 'Hard',
+  image: "https://www.coolgenerator.com/Pic/anime/105363.jpg",
+  name: 'Hanekawa Tsubasa',
+  tags: ['Hanekawa', 'Tsubasa']
+}, {
+  difficulty: 'Hard',
+  image: "https://www.coolgenerator.com/Pic/anime/203555.jpg",
+  name: 'Hikigaya Hachiman',
+  tags: ['Hikigaya', 'Hachiman']
+}, {
+  difficulty: 'Medium',
+  image: "https://www.coolgenerator.com/Pic/anime/307236.jpg",
+  name: 'Dazai Osamu',
+  tags: ['Dazai', 'Osamu']
+}, {
+  difficulty: 'Medium',
+  image: "https://www.coolgenerator.com/Pic/anime/372840.jpg",
+  name: 'Ishigami Yuu',
+  tags: ['Ishigami', 'Yuu']
+}, {
+  difficulty: 'Hard',
+  image: "https://www.coolgenerator.com/Pic/anime/342827.jpg",
+  name: 'Sawada Tsunayoshi',
+  tags: ['Sawada', 'Tsunayoshi']
+}, {
+  difficulty: 'Hard',
+  image: "https://www.coolgenerator.com/Pic/anime/284347.jpg",
+  name: 'Okazaki Tomoya',
+  tags: ['Okazaki', 'Tomoya']
+}, {
+  difficulty: 'Easy',
+  image: "https://www.coolgenerator.com/Pic/anime/28353.jpg",
+  name: 'Simon',
+  tags: ['Simon', 'Simon']
+}, {
+  difficulty: 'Easy',
+  image: "https://www.coolgenerator.com/Pic/anime/78781.jpg",
+  name: 'Hei',
+  tags: ['Hei', 'Hei']
+}, {
+  difficulty: 'Medium',
+  image: "https://www.coolgenerator.com/Pic/anime/55779.jpg",
+  name: 'Fujioka Haruhi',
+  tags: ['Fujioka', 'Haruhi']
+}
+
+]
+module.exports = {
+ get  randomchar() {
+  return data[Math.floor(Math.random() * Math.floor(data.length))] 
+ }
+}
+
